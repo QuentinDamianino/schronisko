386cc2c7f0494fb999eb72a5ace5da07137662d9
QuentinDamianino <brdej998@gmail.com>
2018-08-18 14:01:47 +0200

    small improvements

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Damian
- * Date: 14.08.2018
- * Time: 00:47
- */
 
 namespace AppBundle\Entity;
 
@@ -12,63 +6,59 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
- * Class User
+ * User
+ *
  * @ORM\Table(name="app_users")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  */
-
 class User implements AdvancedUserInterface, \Serializable
 {
     /**
-     * @ORM\Column(type="integer")
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-
     private $id;
 
     /**
-     * @ORM\Column(type="string" , length=25, unique=true)
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=25, unique=true)
      */
-
     private $username;
 
     /**
-     * @ORM\Column(type="string", length=64)
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=64)
      */
-
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=190, unique=true)
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=190, unique=true)
      */
-
     private $email;
 
     /**
-     * @ORM\Column(name="is_active", type="boolean")
+     * @var bool
+     *
+     * @ORM\Column(name="isActive", type="boolean")
      */
-
     private $isActive;
+
 
     public function __construct()
     {
         $this->isActive = true;
     }
 
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
     public function getSalt()
     {
         return null;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
     }
 
     public function getRoles()
@@ -78,6 +68,112 @@ class User implements AdvancedUserInterface, \Serializable
 
     public function eraseCredentials()
     {
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return bool
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
     }
 
     public function isAccountNonExpired()
@@ -120,3 +216,4 @@ class User implements AdvancedUserInterface, \Serializable
             ) = unserialize($serialized);
     }
 }
+
